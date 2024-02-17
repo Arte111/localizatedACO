ed4651b56c9fdfde02fc65492afc6a6d01edac2c
Arte111 <artem.emelianov.2004@gmail.com>
2024-02-17 11:45:52 +0700

    receive better path len

--- ACO.py
+++ ACO.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from numpy.ctypeslib import ndpointer
 import ctypes
+from Graph import Graph
 
 _doublepp = ndpointer(dtype=np.uintp, ndim=1, flags='C')
 
@@ -13,50 +14,6 @@ _step.argtypes = [_doublepp, _doublepp, ctypes.c_size_t, ctypes.c_size_t,
                   ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.POINTER(ctypes.c_double)]
 
 _step.restype = ctypes.POINTER(ctypes.c_size_t)
-
-# from Graph import Graph
-import numpy as np
-from scipy.spatial.distance import cdist
-
-
-class Graph:
-    def __init__(self):
-        self.cords = np.empty(shape=(0, 0), dtype="double")
-        self.distance_matrix = np.empty(shape=(0, 0), dtype="double")
-        self.pheromone_matrix = np.empty(shape=(0, 0), dtype="double")
-
-    def load(self, path, ph=0):
-        temp = []
-        with open(path, 'r') as file:
-            for line in file:
-                temp.append([float(i) for i in line.split(" ")])
-
-        self.cords = np.array(temp)
-        self.distance_matrix = cdist(self.cords, self.cords, 'euclidean')
-        self.pheromone_matrix = np.full((len(self.cords), len(self.cords)), ph, dtype="double")
-
-    def add_k_nearest(self, k):
-        if k >= len(self.cords) - 1:
-            k = len(self.cords) - 2
-        for i in self.distance_matrix:
-            temp = np.sort(i)[k + 1]
-            i[i > temp] = 0
-
-        # recover lost values
-        n = len(self.distance_matrix)
-        for i in range(n):
-            for j in range(i + 1, n):
-                self.distance_matrix[j][i] = max(self.distance_matrix[i][j], self.distance_matrix[j][i])
-                self.distance_matrix[i][j] = self.distance_matrix[j][i]
-
-    def __len__(self):
-        return len(self.cords)
-
-    def evaporation(self, evaporation):
-        self.pheromone_matrix += 1 - evaporation
-
-    def setPH(self, ph):
-        self.pheromone_matrix = np.full((len(self.cords), len(self.cords)), ph, dtype="double")
 
 
 class ACO:
