6aa6f7e93ef90db288bb89886757f653e086675f
Arte111 <artem.emelianov.2004@gmail.com>
2024-03-04 22:17:40 +0700

    regular test

--- Genetic.py
+++ Genetic.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
         "ant_count": range(100, 500, 10),
         "A": [0.25, 0.5, 1, 1.5, 2, 3, 4],
         "B": [3, 4, 5, 6, 7],
-        "Q": range(500, 1000, 50),
+        "Q": range(500, 10000, 500),
         "evap": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
         "start_ph": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
     }
 
-    population_size = 50
+    population_size = 10
     population = []
 
     for i in range(population_size):
@@ -33,20 +33,24 @@ if __name__ == "__main__":
 
     for l in range(20):
         print(f"let's go {l}")
+        s = 0
         for individual in population:
             all_perfomance = [
                 aco.run_performance(ant_count=int(individual["ant_count"]), A=individual["A"], B=individual["B"],
                                     Q=individual["Q"], evap=individual["evap"], start_ph=individual["start_ph"],
-                                    worktime=aco_worktime) for _ in range(5)]
+                                    worktime=aco_worktime) for _ in range(10)]
             individual["performance"] = statistics.mean(all_perfomance)
+            s += individual["performance"]
+            print(individual)
 
-        b = min(population, key=lambda x: x["performance"])
-        print(b)
+        print(f"avg {s // 10}")
+
+        print(min(population, key=lambda x: x["performance"]))
 
         # selection
         temp = []
         for _ in population:
-            temp.append(min(random.choices(population, k=4), key=lambda x: x["performance"]))
+            temp.append(min(random.choices(population, k=2), key=lambda x: x["performance"]))
         population = temp.copy()
 
 
@@ -55,16 +59,20 @@ if __name__ == "__main__":
             for j in ["ant_count", "A", "B", "Q", "evap", "start_ph"]:
                 minj = min(mommy[j], daddy[j])
                 maxj = max(mommy[j], daddy[j])
-                dmin = minj - 0.5 * (maxj - minj)
-                dmax = minj + 0.5 * (maxj - minj)
+                dmin = minj - 1 * (maxj - minj)
+                dmax = minj + 1 * (maxj - minj)
                 child[j] = random.uniform(dmin, dmax)
+
+            for key in child:
+                child[key] = round(abs(child[key]), 2)
+            if child["evap"] > 0.99: child["evap"] = 0.99
 
             return child
 
 
         def mutate(child):
             for i in ["ant_count", "A", "B", "Q", "evap", "start_ph"]:
-                if random.random() < 0.05:
+                if random.random() < 0.15:
                     child[i] = random.triangular(min(parameters[i]), max(parameters[i]),
                                                  random.gauss(child[i], child[i] * 0.2))
 
@@ -81,6 +89,9 @@ if __name__ == "__main__":
             if random.random() < 0.95:
                 offspring.append(crossover(population[j], population[j + 1]))
                 offspring.append(crossover(population[j], population[j + 1]))
+            else:
+                offspring.append(population[j])
+                offspring.append(population[j+1])
 
         map(mutate, offspring)
         population = offspring.copy()
