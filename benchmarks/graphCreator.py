958008d2ffe8e36b613ad7b6ce05da1d959230fc
Arte111 <artem.emelianov.2004@gmail.com>
2024-03-31 20:45:20 +0700

    new

--- benchmarks/graphCreator.py
+++ benchmarks/graphCreator.py
@@ -1,5 +1,7 @@
-from ACO import ACO
 
 if __name__ == "__main__":
-    print("working hard")
-
+    with open("temp.txt") as f1:
+        with open("6d300.txt") as f:
+            for line in f:
+                a, b, c, d, e, f = line.split()
+                print(f"{a} {b} {c} {d} {e}")
